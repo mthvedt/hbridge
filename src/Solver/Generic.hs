2a5e15b5625805dacc4fb51495dc2f319f29e0bd
fpuser9205529 <cynicalkane@gmail.com>
2013-12-24 00:24:20 +0000

    Format-agnostic minimax solving

--- src/Solver/Generic.hs
+++ src/Solver/Generic.hs
@@ -4,47 +4,49 @@ import Data.List
 import Data.Function
 
 -- TODO eq key?
-class (Eq k, Ord s) => OrderedSolvable p m s k | p -> m s k where
+class (Eq k, Ord v) => OrderedSolvable p m s v k | p -> m s v k where
     key :: p -> k
-    rank :: p -> s
+    score :: p -> s
+    scorePlayer :: p -> s -> v
     isFinal :: p -> Bool
     moves :: p -> [(m, p)]
 
-data Nim = Nim {player :: Bool, nimber :: Int}
+data Nim = Nim Bool Int
     deriving (Eq, Show)
 
 legal (Nim _ i) = i >= 0
 move (Nim p i) j = Nim (not p) (i - j)
 
     -- A test class for OrderedSolvable
-instance OrderedSolvable Nim Int Int Nim where
+instance OrderedSolvable Nim Int Int Int Nim where
     key = id
-    rank (Nim p i) =
+    score (Nim p i) =
         case i of
             0 -> if p then 1 else -1
+            1 -> if p then 1 else -1
             _ -> 0
+    scorePlayer (Nim p _) i = if p then i else -i
     isFinal (Nim _ i) = i `elem` [0, 1]
     moves n = filter (legal . snd) $ map (\i -> (i, move n i)) [2, 3, 4]
 
--- minimax1 :: (OrderedSolvable p m s k) => Bool -> p -> s
-minimax1 player pos =
+-- minimax1 :: (OrderedSolvable pos m s v k) => pos -> s
+minimax1 pos =
     if isFinal pos
-    then rank pos
-    else (if player then maximum else minimum) $ map (minimax1 (not player) . snd) $ moves pos
+    then score pos
+    else maximumBy (compare `on` scorePlayer pos) $ map (minimax1 . snd) $ moves pos
     
 -- minimax :: (OrderedSolvable p m s k) => Bool -> p -> (m, p)
-minimax player pos =
-    (if player then maximumBy else minimumBy) (compare `on` minimax1 (not player) . snd) $ moves pos
+minimax pos =
+    maximumBy (compare `on` scorePlayer pos . minimax1 . snd) $ moves pos
 
-playGame :: Nim -> IO ()
+-- playGame :: Nim -> IO ()
 playGame pos =
     if isFinal pos
     then do
         putStrLn $ "Final position: " ++ show pos
-        putStrLn $ "Final score: " ++ show (rank pos)
-        putStrLn $ "Winner: " ++ show (player pos)
+        putStrLn $ "Final score: " ++ show (score pos)
     else do
-        let (m, newPos) = minimax (player pos) pos
+        let (m, newPos) = minimax pos
         putStrLn $ "Position: " ++ show pos
         putStrLn $ "Move: " ++ show m
         playGame newPos
