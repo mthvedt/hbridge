c8030a7ad0bc7a2bf2a4ef2e3e994bc98eba3f04
fpuser9205529 <cynicalkane@gmail.com>
2013-12-26 21:29:48 +0000

    Checkpoint: HashTable and ST monad

--- src/Solver/Generic.hs
+++ src/Solver/Generic.hs
@@ -5,6 +5,8 @@ import Data.Function
 import Control.Monad
 import Control.Monad.ST
 import Data.STRef
+import qualified Data.HashTable.Class as H
+import qualified Data.HashTable.ST.Basic as HST
 
 -- A two player zero sum game with alternating turns.
 -- p: a position type
@@ -44,17 +46,19 @@ instance GameTree Nim Int Int Nim where
             _ -> 0
     isFinal (Nim _ i) = i `elem` [0, 1]
     moves n = filter (legal . snd) $ map (\i -> (i, move n i)) [2, 3, 4]
-
+    
 scoref f pos =
     if isFinal pos
     then score pos
     else f pos
 
-scorefM :: (GameTree p m s k) => (p -> ST x s) -> p -> ST x s
-scorefM f pos =
+type HashTable x k v = HST.HashTable x k v
+
+scorefST :: (GameTree p m s k) => (HashTable x k s -> p -> ST x s) -> HashTable x k s -> p -> ST x s
+scorefST f t pos =
     if isFinal pos
     then return $ score pos
-    else f pos
+    else f t pos
 
 minimax :: (GameTree p m s k) => p -> s
 minimax = scoref $ \pos -> maximum $ map maximin $ children pos
@@ -62,23 +66,25 @@ minimax = scoref $ \pos -> maximum $ map maximin $ children pos
 maximin :: (GameTree p m s k) => p -> s
 maximin = scoref $ \pos -> maximum $ map minimax $ children pos
 
-minimaxM :: (GameTree p m s k) => p -> ST x s
-minimaxM = scorefM $ \pos -> liftM maximum $ mapM maximinM $ children pos
+minimaxST :: (GameTree p m s k) => HashTable x k s -> p -> ST x s
+minimaxST = scorefST $ \t pos -> liftM maximum $ mapM (maximinST t) $ children pos
 
-maximinM :: (GameTree p m s k) => p -> ST x s
-maximinM = scorefM $ \pos -> liftM minimum $ mapM minimaxM $ children pos
+maximinST :: (GameTree p m s k) => HashTable x k s -> p -> ST x s
+maximinST = scorefST $ \t pos -> liftM minimum $ mapM (minimaxST t) $ children pos
 
 solveWith :: (GameTree p m s k) => (p -> s) -> p -> (m, s)
 solveWith solvef pos =
     (if player pos then maximumBy else minimumBy) (compare `on` snd) $ zip (map fst $ moves pos) (map solvef $ children pos)
 
-solveWithM :: (GameTree p m s k) => (p -> ST x s) -> p -> ST x (m, s)
-solveWithM solvef pos = do
-    solutions <- mapM solvef $ children pos
+solveWithM :: (GameTree p m s k) => (HashTable x k s -> p -> ST x s) -> HashTable x k s -> p -> ST x (m, s)
+solveWithM solvef t pos = do
+    solutions <- mapM (solvef t) $ children pos
     return $ (if player pos then maximumBy else minimumBy) (compare `on` snd) $ zip (map fst $ moves pos) solutions
 
-runSolveWith :: (GameTree p m s k) => (forall x. (p -> ST x s)) -> p -> (m, s)
-runSolveWith solvef pos = runST $ solveWithM solvef pos
+runSolveWith :: (GameTree p m s k) => (forall x. (HashTable x k s -> p -> ST x s)) -> p -> (m, s)
+runSolveWith solvef pos = runST $ do
+    t <- HST.new
+    solveWithM solvef t pos
 
 -- playGame :: Nim -> IO ()
 
@@ -88,7 +94,8 @@ playGame pos =
         putStrLn $ "Final position: " ++ show pos
         putStrLn $ "Final score: " ++ show (score pos)
     else do
-        let (m, s) = runSolveWith minimaxM pos
+        -- let (m, s) = solveWith minimax pos
+        let (m, s) = runSolveWith minimaxST pos
         putStrLn $ "Position: " ++ show pos
         putStrLn $ "Move: " ++ show m
         -- putStrLn $ "Prinicpal variation: " ++ show ms
